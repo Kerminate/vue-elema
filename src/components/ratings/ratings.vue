e006fb2a285c2992212381e7cb8c68202d363a1c
Kerminate <kepeilin@outlook.com>
2017-10-12 21:47:32 +0800

    food complete

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,12 +1,168 @@
 <template>
-  <div class="header">
-    我是ratings
+  <div class="ratings">
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">{{seller.score}}</h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
+      </div>
+      <split></split>
+      <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="ratings" @select="selectRating" @toggle="toggleContent"></ratingselect>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-for="rating in ratings" class="rating-item">
+            <div class="avatar">
+              <img :src="rating.avatar">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                <span class="icon-thumb_up"></span>
+                <span v-for="item in rating.recommend">{{item}}</span>
+              </div>
+              <div class="time">
+                {{rating.rateTime | format}}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmasctipt-6">
-export default {}
+  import star from '@/components/star/star'
+  import split from '@/components/split/split'
+  import ratingselect from '@/components/ratingselect/ratingselect'
+  import {formatDate} from '@/common/js/date'
+
+  const ALL = 2
+  const ERR_OK = 0
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        ratinfs: [],
+        selectType: ALL,
+        onlyContent: true
+      }
+    },
+    created () {
+      this.$http.get('/api/ratings').then((response) => {
+        let result = response.body
+        if (response.errno === ERR_OK) {
+          this.ratings = result.data
+        }
+      })
+    },
+    filters: {
+      format (time) {
+        let date = new Date(time)
+        return formatDate(date, 'yyyy-MM-dd hh:mm')
+      }
+    },
+    components: {
+      star,
+      split,
+      ratingselect
+    }
+  }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
+<style lang="stylus">
+  .ratings
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    overflow: hidden
+    .overview
+      display: flex
+      padding: 18px 0
+      .overview-left
+        flex: 0 0 137px
+        width: 137px
+        padding: 6px 0
+        border-right: 1px solid rgba(7, 17, 27, 0.1)
+        text-align: center
+        @media only screen and (max-width: 320px)
+          flex: 0 0 120px
+          width: 120px
+        .score
+          margin-bottom: 6px
+          line-height: 28px
+          font-size: 24px
+          color: rgb(255, 153, 0)
+        .title
+          margin-bottom: 8px
+          line-height: 12px
+          font-size: 12px
+          color: rgb(7, 17, 27)
+        .rank
+          line-height: 10px
+          font-size: 10px
+          color: rgb(147, 153, 159)
+      .overview-right
+        flex: 1
+        padding: 6px 0 6px 24px
+        @media screen and (max-width: 320px)
+          padding-left: 6px
+        .score-wrapper
+          margin-bottom: 8px
+          font-size: 0
+          .title
+            display: inline-block
+            vertical-align: top
+            line-height: 18px
+            font-size: 12px
+            color: rgb(7, 17, 27)
+          .star
+            display: inline-block
+            vertical-align: top
+            margin: 0 12px
+          .score
+            display: inline-block
+            vertical-align: top
+            line-height: 18px
+            font-size: 12px
+            color: rgb(255, 153, 0)
+        .delivery-wrapper
+          font-size: 0
+          .title
+            line-height: 18px
+            font-size: 12px
+            color: rgb(7, 17, 27)
+          .delivery
+            margin-left: 12px
+            font-size: 12px
+            color: rgb(147, 153, 159)
 </style>
--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,11 @@
 <template lang="html">
   <div class="ratingselect">
     <div class="rating-type border-1px">
-      <span>{{desc.all}}</span>
-      <span>{{desc.positive}}</span>
-      <span>{{desc.negative}}</span>
+      <span @click="select(2, $event)" class="block positive" :class="{'active': selectType === 2}">{{desc.all}}<span class="count">{{ratings.length}}</span></span>
+      <span @click="select(0, $event)" class="block positive" :class="{'active': selectType === 0}">{{desc.positive}}<span class="count">{{positives.length}}</span></span>
+      <span @click="select(1, $event)" class="block negative" :class="{'active': selectType === 1}">{{desc.negative}}<span class="count">{{negatives.length}}</span></span>
     </div>
-    <div class="switch">
+    <div @click="toggleContent" class="switch" :class="{'on': onlyContent}">
       <span class="icon-check_circle"></span>
       <span class="text">只看有内容的评价</span>
     </div>
@@ -13,8 +13,8 @@
 </template>
 
 <script>
-  // const POSITIVE = 0
-  // const NEGATIVE = 1
+  const POSITIVE = 0
+  const NEGATIVE = 1
   const ALL = 2
 
   export default {
@@ -43,6 +43,28 @@
           }
         }
       }
+    },
+    methods: {
+      select (type, event) {
+        // this.selectType = type
+        this.$emit('select', type)
+      },
+      toggleContent () {
+        // this.onlyContent = !this.onlyContent
+        this.$emit('toggle')
+      }
+    },
+    computed: {
+      positives () {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === POSITIVE
+        })
+      },
+      negatives () {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === NEGATIVE
+        })
+      }
     }
   }
 </script>
@@ -55,4 +77,44 @@
       padding: 18px 0
       margin: 0 18px
       border-1px(rgba(7, 17, 27, 0.1))
+      font-size: 0
+      .block
+        display: inline-block
+        padding: 8px 12px
+        margin-right: 8px
+        border-radius: 1px
+        line-height: 16px
+        font-size: 12px
+        color: rgb(77, 85, 93)
+        &.active
+          color: #fff
+        .count
+          margin-left: 2px
+          font-size: 8px
+        &.positive
+          background: rgba(0, 160, 200, 0.2)
+          &.active
+            background: rgba(0, 160, 200, 1)
+        &.negative
+          background: rgba(77, 85, 93, 0.2)
+          &.active
+            background: rgba(77, 85, 93, 1)
+    .switch
+      padding: 12px 18px
+      line-height: 24px
+      border-bottom:1px  solid rgba(7, 17, 27, 0.1)
+      color: rgb(147, 153, 159)
+      font-size: 0
+      &.on
+        .icon-check_circle
+          color: #00c850
+      .icon-check_circle
+        display: inline-block
+        vertical-align: top
+        margin-right: 4px
+        font-size: 24px
+      .text
+        display: inline-block
+        vertical-align: top
+        font-size: 12px
 </style>
